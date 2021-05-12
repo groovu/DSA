ecbf3ed09096c755dc2419fbe72c8ec6651f6ac0
Cherish Truong <43489192+groovu@users.noreply.github.com>
2021-05-12 14:05:48 -0700

    Time: 68 ms (52.31%), Space: 14.3 MB (54.07%) - LeetHub

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,18 +1,36 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if len(s) == 1:
-            return 1
-        longest_so_far = 0
-        for i in range(len(s)):
-            runner = i
-            seen = set()
-            while runner < len(s):
-                if s[runner] not in seen:
-                    seen.add(s[runner])
-                    runner += 1
-                else:
-                    break
-            if (runner - i) > longest_so_far:
-                longest_so_far = runner - i
-            runner = len(s)
-        return longest_so_far
+        # if len(s) == 1:
+        #     return 1
+        # longest_so_far = 0
+        # for i in range(len(s)):
+        #     runner = i
+        #     seen = set()
+        #     while runner < len(s):
+        #         if s[runner] not in seen:
+        #             seen.add(s[runner])
+        #             runner += 1
+        #         else:
+        #             break
+        #     if (runner - i) > longest_so_far:
+        #         longest_so_far = runner - i
+        #     runner = len(s)
+        # return longest_so_far
+        if 0 <= len(s) <= 1:
+            return len(s)
+        longest = 0
+        left = 0
+        right = 0
+        seen = set()
+        while right < len(s):
+            char = s[right]
+            if char not in seen:
+                seen.add(char)
+                longest = max(longest, right - left + 1)
+                right += 1
+            else:
+                seen.remove(s[left])
+                left += 1
+            
+        return longest
+            
