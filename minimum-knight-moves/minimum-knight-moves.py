1e90108f2e6ebe6f739e6ba6519dffe7b87b5f96
Cherish Truong <43489192+groovu@users.noreply.github.com>
2021-05-13 08:49:42 -0700

    Time: 6100 ms (26.22%), Space: 76.5 MB (17.79%) - LeetHub

---
+++ minimum-knight-moves/minimum-knight-moves.py
@@ -0,0 +1,43 @@
+class Solution:
+    def minKnightMoves(self, x: int, y: int) -> int:
+        # the offsets in the eight directions
+        offsets = [(1, 2), (2, 1), (2, -1), (1, -2),
+                   (-1, -2), (-2, -1), (-2, 1), (-1, 2)]
+
+        def bfs(x, y):
+            visited = set()
+            queue = deque([(0, 0)])
+            steps = 0
+
+            while queue:
+                curr_level_cnt = len(queue)
+                # iterate through the current level
+                for i in range(curr_level_cnt):
+                    curr_x, curr_y = queue.popleft()
+                    if (curr_x, curr_y) == (x, y):
+                        return steps
+
+                    for offset_x, offset_y in offsets:
+                        next_x, next_y = curr_x + offset_x, curr_y + offset_y
+                        if (next_x, next_y) not in visited:
+                            visited.add((next_x, next_y))
+                            queue.append((next_x, next_y))
+
+                # move on to the next level
+                steps += 1
+
+        return bfs(x, y)
+        # list out all possible moves
+        # create search algo
+            # keep track of visited
+            # initialize deque
+            # track number of steps
+            # while queue:
+            # for i in range len(queue) represents iterating through all items in queue
+            # first time is 1
+            # second time is 8
+            # third time is 8 * 8 - visited nodes
+            # ...
+            
+        
+        
