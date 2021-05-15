3381a983213acb99d6ea7838a92d6894dbe979cb
Cherish Truong <43489192+groovu@users.noreply.github.com>
2021-05-15 10:06:03 -0700

    Time: 32 ms (55.20%), Space: 14.2 MB (76.72%) - LeetHub

--- robot-bounded-in-circle/robot-bounded-in-circle.py
+++ robot-bounded-in-circle/robot-bounded-in-circle.py
@@ -1,28 +1,24 @@
 class Solution:
     def isRobotBounded(self, instructions: str) -> bool:
-        horz = []
-        vert = []
-        dir = 0
-        # let 0123 = n,e,s,w
+        instr = instructions * 4
+        x = y = 0
+        direction = 0
         
-        for i in instructions:
-            if i == 'G':
-                #increment horz or vert depending on direction
-                if dir == 0:
-                    vert.append(1)
-                if dir == 1:
-                    horz.append(1)
-                if dir == 2:
-                    vert.append(-1)
-                if dir == 3:
-                    horz.append(-1)
-            if i == 'L':
-                dir = (dir + 3) % 4
-            if i == 'R':
-                # change dir
-                dir = (dir + 1) % 4
+        for i in instr:
+            if i == "G":
+                if direction == 0:
+                    y += 1
+                if direction == 1:
+                    x += 1
+                if direction == 2:
+                    y -= 1
+                if direction == 3:
+                    x -= 1
+            if i == "L":
+                direction = (direction - 1) % 4
+            if i == "R":
+                direction = (direction + 1) % 4
         
-        posX = sum(horz)
-        posY = sum(vert)
-        
-        return (posX == 0 and posY == 0) or dir != 0
+        if (x == 0 and y == 0):
+            return True
+        return False
